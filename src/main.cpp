5bf7d57b40f738fe645943b041efd0d62d350744
TuMeiRBQ <1043585877@qq.com>
2022-11-07 21:39:49 +0800

    dev

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * @Author: TuMeiRBQ 1043585877@qq.com
+ * @Date: 2022-11-07 21:38:00
+ * @LastEditors: TuMeiRBQ 1043585877@qq.com
+ * @LastEditTime: 2022-11-07 21:39:03
+ * @FilePath: /MOI_X/src/main.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    int aa = 10;
+	std::cout << "This is moi-x!" << std ::endl;
+    std::cout << "aa: "<< aa << std ::endl;
+	return 0;
+
+}
